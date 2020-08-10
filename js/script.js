3dff37ac8725fec3a565be150e312ae910401926
Mateusz <grudzinski.mateusz4@gmail.com>
2020-08-10 15:03:53 +0200

    zadanie 5.3

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = `papierp`;
+
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
