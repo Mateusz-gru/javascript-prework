a7fcd21514487e13a9183b0e5b30901b2e35e761
Mateusz <grudzinski.mateusz4@gmail.com>
2020-08-10 22:46:54 +0200

    odczytanie ruchu komputera
    Dodanie trzech opcji dla ruchu komputera 1:kamień, 2:papier, 3:nożyczki

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if (randomNumber == 2){
+    computerMove = 'papier';  
+}
+else if (randomNumber == 3){
+    computerMove = 'nożyczki';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = `papierp`;
-
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+printMessage('Mój ruch to: ' + computerMove);
