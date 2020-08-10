7af948aaea8ba6f2a1a9aba3c276436ab9adba54
Mateusz <grudzinski.mateusz4@gmail.com>
2020-08-10 23:40:01 +0200

    Wynik gry
    Wszystkie możliwe wariany wyników gry

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Odczytanie ruchu komputera
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -15,6 +17,8 @@ else if (randomNumber == 3){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// Odczytanie ruchu gracza
 
 HTML JSResult
 EDIT ON
@@ -37,3 +41,42 @@ printMessage('Twój ruch to: ' + playerMove);
 Run Pen 
 
 Resources1×0.5×0.25×Rerun
+
+// Wynik gry
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('Nieznany ruch');
+}
+if( computerMove == 'kamień' && playerMove == 'nożyczki'){
+  printMessage('Ty przegrywasz!');
+}
+else( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+if( computerMove == 'papier' && playerMove == 'nożyczki'){
+  printMessage('Ty wygrywasz!');
+}
+else( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+  printMessage('Nieznany ruch');
+}
+if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przegrywasz!');
+}
+else( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+if( computerMove == 'nożyczki' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else( computerMove == 'nożyczki' && playerMove == 'nieznany ruch'){
+  printMessage('Nieznany ruch');
+}
+if( computerMove == 'nożyczki' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+}
+else( computerMove == 'nożyczki' && playerMove == 'nożyczki'){
+  printMessage('Remis!');
+}
